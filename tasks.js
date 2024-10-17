578b1db3c1f02031017f91a6842630c7f507b133
sci-mat <sci-mat@bmu.edu.in>
2024-10-17 08:40:31 +0530

    Task commit

---
+++ tasks.js
@@ -0,0 +1,55 @@
+/*
+Task 1: Longest Substring without Repeating characters problem
+
+
+(Submission can be made in any langauge comfortable, Just create a file with the solution code )
+
+
+ Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+
+
+Task Deadline: 21st October 2024
+
+Cheatsheet: 
+
+1. git add .
+
+2. git commit -m "your_message"
+
+3. git push origin main
+
+
+P.S.: your submission in the pull request should only be made in the form of yourName_batch
+
+
+
+
+
+*/
